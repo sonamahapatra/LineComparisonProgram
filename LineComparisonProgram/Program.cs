f0ce819f4c315b0efe601077641b5054589a3afd
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-09 20:42:55 +0530

    [sonamahapatra] Adding Welcome Message

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -8,10 +8,25 @@ namespace LineComparisonProgram
 {
     internal class Program
     {
+        double firstLine, secondLine;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Line Comparison Program");
-            LineComp.FindLine();
+           // LineComp.FindLine();
+            LineComp line = new LineComp(3, 4, -2, 3);
+            firstLine = line.Calculate();
+            LineComp line1 = new LineComp(1, 2, 3, 4);
+            secondLine = line1.Calculate();
+
+            //UC-2
+            if (firstLine.Equals(secondLine))
+            {
+                Console.WriteLine("Two lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are Not equal");
+            }
             Console.ReadLine();
         }
     }
