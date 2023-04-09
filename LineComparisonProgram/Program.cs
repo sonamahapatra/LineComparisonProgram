7c1aa45bd20005fd7b6ca66e7383402cf7a93718
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-09 19:25:25 +0530

    [sonamahapatra] Added Welcome Message

---
+++ LineComparisonProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Line Comparison Program");
+            Console.ReadLine();
+        }
+    }
+}
