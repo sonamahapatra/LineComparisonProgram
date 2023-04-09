eb8f541b795ef67c8a20dfe87bdad187431b870d
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-09 21:16:28 +0530

    [sonamahapatra] Compairing Lines

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -12,8 +12,26 @@ namespace LineComparisonProgram
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Line Comparison Program");
-            Line line = new Line(1, 2, 3, 4);
-            line.Length();
+            Line line = new Line(1, 2, 3, 5);
+            double length1 = line.Length();
+            Console.WriteLine("Length of 1st Line : " + length1);
+
+            Line line2 = new Line(1, 2, 3, 4);
+            double length2 = line2.Length();
+            Console.WriteLine("Length of 2nd Line : " + length2);
+
+            if (length1.CompareTo(length2) == 0)
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else if (length1.CompareTo(length2) > 0)
+            {
+                Console.WriteLine("1st Line is greter");
+            }
+            else
+            {
+                Console.WriteLine("2nd Line is greter");
+            }
             Console.ReadLine();
             
         }
